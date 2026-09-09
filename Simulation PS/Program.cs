3c07954336c0c04357a46eb6eb149793fe840076
Amr <AmrAhmed24555@gmail.com>
2026-09-10 02:26:50 +0300

    Assignment 1 1. Two Sum

--- Simulation PS/Program.cs
+++ Simulation PS/Program.cs
@@ -3,7 +3,33 @@ namespace Simulation_PS
 {
     internal class Program
     {
+        // Assignment 1
+        public int[] TwoSum(int[] nums, int target)
+        {
 
+            Dictionary<int, int> found = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                found[nums[i]] = i;
+            }
+
+            int l = 0, r = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int x = target - nums[i];
+
+                if (found.ContainsKey(x) && found[x] != i)
+                {
+                    l = i;
+                    r = found[x];
+                    break;
+                }
+            }
+
+            return new int[] { l, r };
+        }
 
         static void Main(string[] args)
         {
